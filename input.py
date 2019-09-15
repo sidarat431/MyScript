50493fa65fe50f1a72e653d1651be727d61de903
sidarat431 <52315140+sidarat431@users.noreply.github.com>
2019-09-15 22:39:53 +0700

    Add files via upload

---
+++ input.py
@@ -0,0 +1,6 @@
+# Output a string and a	variable value. 
+print("Welcome"	,user)
+#Initialize another variable with a user-specified value. 
+lang = input("Favorite programming language?:")
+# Output a string and a	variable value. 
+print(lang,"Is"	,"Fun",	sep="*",end ="!\n") 
